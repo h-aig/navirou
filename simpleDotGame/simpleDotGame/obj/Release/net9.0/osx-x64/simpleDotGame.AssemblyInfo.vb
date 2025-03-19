e3e8a8bdb5ea210a2a6d55520a8930cf0f12283b
henry <101145848+h-aig@users.noreply.github.com>
2025-03-19 21:47:34 +0000

    added separate binaries! will go in releases, also updated readme

--- simpleDotGame/simpleDotGame/obj/Release/net9.0/osx-x64/simpleDotGame.AssemblyInfo.vb
+++ simpleDotGame/simpleDotGame/obj/Release/net9.0/osx-x64/simpleDotGame.AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("simpleDotGame"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4203ff44559f38e35592ffef066a64079ae64d"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e91dc3d9b0c97a09e833a03a0b29d1d22769e58"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("simpleDotGame"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("simpleDotGame"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")> 
